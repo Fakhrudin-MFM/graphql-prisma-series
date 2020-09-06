62ec27406ce84599c00748c41ebc7617b6a06adf
John Curry <fastpenguin91@gmail.com>
2020-09-06 16:45:36 -0700

    finished graphql queries

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let users = [
+  {
+    id: "123",
+    firstName: "Cindy",
+    email: "cindy@cindy.com",
+  },
+  {
+    id: "456",
+    firstName: "Todd",
+    email: "todd@todd.com",
+  },
+];
+
 const typeDefs = `
 type Query {
   helloWorld: String!
+  users: [User!]!
+  user(userId: ID!): User
+}
+
+type User {
+    id: ID!
+    firstName: String!
+    email: String!
 }
 `;
 
 const resolvers = {
   Query: {
     helloWorld: () => `Hello World! What a day!`,
+    // users: () => users, // We could do this since it's such a simple query
+    users: (parent, args, context, info) => {
+      return users;
+    },
+    user: (parent, args, context, info) => {
+      return users.find((user) => {
+        if (user.id == args.userId) {
+          return user;
+        }
+      });
+    },
+  },
+  User: {
+    id: (parent) => parent.id,
+    firstName: (parent) => {
+      console.log("What is parent: ", parent);
+      return parent.firstName;
+    },
+    // firstName: (parent) => parent.firstName, // simple version. above to illustrate
+    email: (parent) => parent.email,
   },
 };
 
